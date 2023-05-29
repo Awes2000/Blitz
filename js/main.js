1e5f5bed2aeea38142c220026ababc78e3fa2149
Awes <28003@ma-web.nl>
2023-05-29 17:07:40 +0200

    Meer dailypaper styling en kleur toegepast

--- js/main.js
+++ js/main.js
@@ -26,3 +26,46 @@ for (let i = 0; i < arrowButtons.length; i++) {
   };
 }
 /*Reviews einde */
+
+window.addEventListener("load", function () {
+  const heroImage = document.querySelector(".hero-image");
+  heroImage.classList.add("loaded");
+});
+
+// Get the hero image element
+const heroImage = document.getElementById("hero-image");
+
+// Create a ripple animation using anime.js
+anime({
+  targets: heroImage,
+  translateX: ["-100%", "0%"],
+  opacity: [0, 1],
+  easing: "linear",
+  duration: 2000, // Adjust the duration as desired
+});
+
+/*SHOWROOM BLITZ */
+// Automatically start the carousel
+window.addEventListener("load", function () {
+  startCarousel();
+});
+
+function startCarousel() {
+  setInterval(nextSlide, 5000); // Transition every 5 seconds
+}
+
+function nextSlide() {
+  const carouselImages = document.querySelector(".carousel-images");
+  const imageWidth = carouselImages.firstElementChild.clientWidth;
+
+  carouselImages.style.transition = "transform 1s ease-in-out";
+  carouselImages.style.transform = `translateX(-${imageWidth}px)`;
+
+  setTimeout(function () {
+    carouselImages.appendChild(carouselImages.firstElementChild);
+    carouselImages.style.transition = "none";
+    carouselImages.style.transform = "translateX(0)";
+  }, 1000);
+}
+
+/* SHOWROOM BLITZ END */
